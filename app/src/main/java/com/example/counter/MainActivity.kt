a14d62eec50f8665174f9a364215e12a5fd47195
Harshit Tyagi <51489561+Tyagism@users.noreply.github.com>
2024-04-08 12:40:04 +0530

    Basic Functionality Created With Basic User Interface.

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -1,116 +1,102 @@
-package com.example.counter;
+package com.example.counter
 
-import static com.example.counter.R.id.buttoncount;
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ScrollView
+import android.widget.TextView
+import android.widget.ToggleButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.chip.Chip
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+class MainActivity : AppCompatActivity() {
+    private var nowCount = 0
+    private var todayCount = 0
+    private var lifetimeCount = 0
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.material.chip.Chip;
-
-public class MainActivity extends AppCompatActivity {
-
-    public int NowCount = 0;
-    public int todayCount = 0;
-    public int lifetimeCount = 0;
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val scrollView = findViewById<ScrollView>(R.id.scrollview)
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        SharedPreferences preferences = getSharedPreferences("com.example.counter", Context.MODE_PRIVATE);
-        boolean isSwitch1On = preferences.getBoolean("switch1", false);
-        TextView currentCount = findViewById(R.id.current_chip);
+        val preferences = getSharedPreferences("com.example.counter", MODE_PRIVATE)
+        val isSwitch1On = preferences.getBoolean("switch1", false)
+        val currentCount = findViewById<TextView>(R.id.current_chip)
         if (isSwitch1On) {
-            currentCount.setVisibility(View.GONE);
-
+            currentCount.visibility = View.GONE
         }
 
-        SharedPreferences preference = this.getSharedPreferences("com.example.counter", Context.MODE_PRIVATE);
+        val preference = getSharedPreferences("com.example.counter", MODE_PRIVATE)
         if (preference.getBoolean("firstRun", true)) {
-            startActivity(new Intent(this, SetupActivity.class));
+            startActivity(Intent(this, SetupActivity::class.java))
         }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-
-        });
-
-        @SuppressLint("CutPasteId")
-
-        final Chip currentChip = findViewById(R.id.current_chip);
-        final Chip todayChip = findViewById(R.id.today_chip);
-        final Chip lifetimeChip = findViewById(R.id.lifetime_chip);
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
-        Button button = findViewById(buttoncount);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                if (isSwitch1On) {
-                    todayCount++;
-                    lifetimeCount++;
-                } else {
-                    NowCount++;
-                    if (NowCount > 108) {
-                        NowCount = 1;
-                        todayCount++;
-                        lifetimeCount++;
-                    }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v: View, insets: WindowInsetsCompat ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        @SuppressLint("CutPasteId") val currentChip = findViewById<Chip>(R.id.current_chip)
+        val todayChip = findViewById<Chip>(R.id.today_chip)
+        val lifetimeChip = findViewById<Chip>(R.id.lifetime_chip)
+        @SuppressLint("MissingInflatedId", "LocalSuppress") val button =
+            findViewById<Button>(R.id.buttoncount)
+        val toggleButton = findViewById<ToggleButton>(R.id.toggleButton)
+        button.setOnClickListener {
+            if (isSwitch1On) {
+                todayCount++
+                lifetimeCount++
+            } else {
+                nowCount++
+                if (nowCount > 108) {
+                    nowCount = 1
+                    todayCount++
+                    lifetimeCount++
                 }
-                currentChip.setText("Current Count: " + NowCount);
-                todayChip.setText("Today's Count: " + todayCount);
-                lifetimeChip.setText("Lifetime Count: " + lifetimeCount);
             }
-        });
-
-        CardView settings = findViewById(R.id.settings);
-
-        settings.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                if (isSwitch1On) {
-                    NowCount++;
-                    if (NowCount > 108) {
-                        NowCount = 1;
-                        todayCount++;
-                        lifetimeCount++;
-                        currentCount.setVisibility(View.VISIBLE);
-
-                    } else if (isSwitch1On) {
-                        
+            currentChip.text = "Current Count: $nowCount"
+            todayChip.text = "Today's Count: $todayCount"
+            lifetimeChip.text = "Lifetime Count: $lifetimeCount"
+        }
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                currentChip.visibility = View.GONE
+                button.setOnClickListener {
+                    if (isChecked) {
+                        todayCount++
+                        lifetimeCount++
                     }
-                    {
-                        currentCount.setVisibility(View.GONE);
-
-                    }
+                    todayChip.text = "Today's Count: $todayCount"
+                    lifetimeChip.text = "Lifetime Count: $lifetimeCount"
                 }
+            } else {
+                currentChip.visibility = View.VISIBLE
+                button.setOnClickListener {
+                    if (!isChecked) {
+                        nowCount++
+                        if (nowCount > 108) {
+                            nowCount = 1
+                            todayCount++
+                            lifetimeCount++
+                        }
+                    }
+                    currentChip.text = "Current Count: $nowCount"
+                    todayChip.text = "Today's Count: $todayCount"
+                    lifetimeChip.text = "Lifetime Count: $lifetimeCount"
+                } } }
 
 
-                currentChip.setText("Current Count: " + NowCount);
-                todayChip.setText("Today's Count: " + todayCount);
-                lifetimeChip.setText("Lifetime Count: " + lifetimeCount);
-            }
-        });
-    }
-}
 
+
+
+
+
+
+    } }
--- app/src/main/java/com/example/counter/SetupActivity.kt
+++ app/src/main/java/com/example/counter/SetupActivity.kt
@@ -1,84 +1,62 @@
-package com.example.counter;
+package com.example.counter
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.ProgressBar
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.Switch;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
-
-public class SetupActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup);
-
-        SharedPreferences preferences = getSharedPreferences("com.example.counter", Context.MODE_PRIVATE);
+class SetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+        var preferences = getSharedPreferences("com.example.counter", MODE_PRIVATE)
         if (!preferences.getBoolean("firstRun", true)) {
-            startActivity(new Intent(SetupActivity.this, MainActivity.class));
-            finishAffinity();
-            return;
+            startActivity(Intent(this@SetupActivity, MainActivity::class.java))
+            finishAffinity()
+            return
         }
-
-
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
-
-        Button finishbtn = findViewById(R.id.finishbtn);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
-
-        @SuppressLint("UseSwitchCompatOrMaterialCode")
-        Switch switch1 = findViewById(R.id.switch1);
-
-        @SuppressLint("UseSwitchCompatOrMaterialCode")
-        Switch switch2 = findViewById(R.id.switch2);
-
-
-        preferences = getSharedPreferences("com.example.counter", Context.MODE_PRIVATE);
-        SharedPreferences finalPreferences1 = preferences;
-        switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            SharedPreferences.Editor editor = finalPreferences1.edit();
-            editor.putBoolean("switch1", isChecked);
-
-            editor.apply();
-        });
-
-
-        boolean isDarkModeOn = preferences.getBoolean("darkMode", false);
-        switch2.setChecked(isDarkModeOn);
-
-        SharedPreferences finalPreferences2 = preferences;
-        switch2.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            SharedPreferences.Editor editor = finalPreferences2.edit();
-            editor.putBoolean("darkMode", isChecked);
-            editor.apply();
-
+        @SuppressLint("MissingInflatedId", "LocalSuppress") val finishbtn =
+            findViewById<Button>(R.id.finishbtn)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        @SuppressLint("UseSwitchCompatOrMaterialCode") val switch1 =
+            findViewById<Switch>(R.id.switch1)
+        @SuppressLint("UseSwitchCompatOrMaterialCode") val switch2 =
+            findViewById<Switch>(R.id.switch2)
+        preferences = getSharedPreferences("com.example.counter", MODE_PRIVATE)
+        val finalPreferences1 = preferences
+        switch1.setOnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean ->
+            val editor = finalPreferences1.edit()
+            editor.putBoolean("switch1", isChecked)
+            editor.apply()
+        }
+        val isDarkModeOn = preferences.getBoolean("darkMode", false)
+        switch2.setChecked(isDarkModeOn)
+        val finalPreferences2 = preferences
+        switch2.setOnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean ->
+            val editor = finalPreferences2.edit()
+            editor.putBoolean("darkMode", isChecked)
+            editor.apply()
             if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
-        });
-
-
-        SharedPreferences finalPreferences = preferences;
-        finishbtn.setOnClickListener(v -> {
-            progressBar.setProgress(100);
-
-            SharedPreferences.Editor editor = finalPreferences.edit();
-            editor.putBoolean("firstRun", false);
-            editor.apply();
-
-            Intent intent = new Intent(SetupActivity.this, MainActivity.class);
-            startActivity(intent);
-            finishAffinity();
-        });
+        }
+        val finalPreferences = preferences
+        finishbtn.setOnClickListener { v: View? ->
+            progressBar.progress = 100
+            val editor = finalPreferences.edit()
+            editor.putBoolean("firstRun", false)
+            editor.apply()
+            val intent = Intent(this@SetupActivity, MainActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+        }
     }
 }
